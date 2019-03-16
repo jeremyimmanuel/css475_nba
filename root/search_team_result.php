f5d56b58fa68a35c640458c8f3d285bdeeba7aae
Nathan <newenfield@protonmail.ch>
2019-03-16 15:26:53 -0700

    Add files via upload

--- root/search_team_result.php
+++ root/search_team_result.php
@@ -21,6 +21,7 @@ $team_kw = $_GET["team"];
 $sql = "SELECT  TeamName,
                 TEAM.Wins AS teamWins,
                 TEAM.Losses AS teamLosses,
+                ArenaName,
                 HEAD_COACH.Name AS coachName
 		FROM TEAM
             JOIN ARENA USING (ArenaId)
@@ -33,6 +34,7 @@ if ($result->num_rows > 0) {
     $team_col = $result->fetch_assoc();
 	echo "<h2>" . $team_col["TeamName"] . "</h2><br>";
     echo "2018-19 record: " . $team_col["teamWins"] . " - " . $team_col["teamLosses"] . "<br>";
+    echo "Home Stadium: " . $team_col["ArenaName"] . "<br>";
     echo "Head Coach: " . $team_col["coachName"] . "<br>";
 } else {
     echo "No such team as $team_kw";
@@ -57,7 +59,10 @@ if ($result->num_rows > 0)
 	//	output data of each row
 	while($row = $result->fetch_assoc()) 
 	{
-		echo "<tr><td>" . $row["PlayerName"] . "." . "</td><td>" . $row["DOB"] . "</td><td>". $row["Height"] . "</td><td>". $row["Weight"] . "</td><td>". $row["DraftYear"] . "</td></tr>";
+        if($row["DraftYear"] == NULL)
+            echo "<tr><td>" . $row["PlayerName"] . "</td><td>" . $row["DOB"] . "</td><td>". $row["Height"] . "</td><td>". $row["Weight"] . "</td><td>Undrafted</td></tr>";
+        else    
+            echo "<tr><td>" . $row["PlayerName"] . "</td><td>" . $row["DOB"] . "</td><td>". $row["Height"] . "</td><td>". $row["Weight"] . "</td><td>". $row["DraftYear"] . "</td></tr>";
 	}
 	echo "</table>";
     
